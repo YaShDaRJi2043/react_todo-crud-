6eb7a5cd6b827e850225fa17c26df5eaf7e5a82f
YaShDaRJi2043 <yashdarji1955@gmail.com>
2022-12-01 23:35:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
-import Edit from "./components/Edit";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/edit" element={<Edit />} />
       </Routes>
     </>
   );
